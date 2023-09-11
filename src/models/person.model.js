a47d6c81873d49c00fead5ae40438c5e3fa484d4
Ezeah <ezeahnnamdi@gmail.com>
2023-09-11 03:24:45 +0100

    add model-branch

---
+++ src/models/person.model.js
@@ -0,0 +1,30 @@
+// Define the User(person) Schema
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema(
+  {
+    user_id: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    name: {
+      type: String,
+      required: [true, 'Please add a name'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Please add an email'],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Please add a password'],
+    },
+  },  
+{
+  // Timestamps
+  timestamps: true,
+});
+
+module.exports = mongoose.model('User', userSchema)
