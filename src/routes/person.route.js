0e3065fdb2ed0d1b4d93b90808b194d624002ff1
Ezeah <ezeahnnamdi@gmail.com>
2023-09-11 16:17:08 +0100

    add user-routes modify app.js

---
+++ src/routes/person.route.js
@@ -0,0 +1,22 @@
+const UserController = require('../controllers/person.controller');
+const UserSchema = require('../schemas/person.schema');
+const UserRouter = express.Router();
+const express = require('express');
+
+
+// create a user
+UserRouter.post("/", [validateBody(UserSchema.createUser)], UserController.createUser);
+
+// get a user info with user_id
+UserRouter.get("/:user_id", [validateParams(UserSchema.userId)], UserController.getUserByUserId);
+
+// update user info
+UserRouter.put("/:user_id", [validateParams(UserSchema.userId), validateBody(UserSchema.updateUser)], UserController.updateUser);
+
+// update user info
+UserRouter.patch("/:user_id", [validateParams(UserSchema.userId), validateBody(UserSchema.updateUser)], UserController.updateUser);
+
+// delete user
+UserRouter.delete("/:user_id", [validateParams(UserSchema.userId)], UserController.deleteUser);
+
+module.exports = userTalentRouter;
